058b98d3c1c23f5eade3cf561fded597de2e5b5e
Isaac Peña <isaac.pr99@gmail.com>
2021-04-02 14:42:18 -0400

    feat: add TopBar component to common layout

--- components/layouts/common.tsx
+++ components/layouts/common.tsx
@@ -1,6 +1,19 @@
-import { Box, Center, Container, Flex, Spacer } from "@chakra-ui/layout";
+import { IconButton } from "@chakra-ui/button";
+import { SearchIcon } from "@chakra-ui/icons";
+import { Input, InputGroup, InputLeftElement } from "@chakra-ui/input";
+import {
+  Box,
+  Center,
+  Container,
+  Flex,
+  HStack,
+  Spacer
+} from "@chakra-ui/layout";
 import React from "react";
+import InputSearchTopBar from "../elements/InputSearchTopBar";
 import PlaylistIlustration from "../elements/PlaylistIlustration";
+import ToggleColorMode from "../elements/ToggleColorMode";
+import TopBar from "../modules/TopBar";
 
 type CommonLayoutProps = {
   children: React.ReactNode;
@@ -10,7 +23,8 @@ export default function CommonLayout({ children }: CommonLayoutProps) {
   return (
     <Container maxW='container.xl' display='flex' flexFlow='row'>
       <Flex w='100%' direction='column'>
-        <Center marginBottom='5vh' marginTop='5vh'>
+        <TopBar></TopBar>
+        <Center marginBottom='5vh'>
           <PlaylistIlustration
             style={{
               width: "250px",
