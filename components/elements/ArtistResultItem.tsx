fe4c622c93984f7e5c01e56e58279c11f2c562fc
Isaac Peña <isaac.pr99@gmail.com>
2021-04-02 11:16:43 -0400

    feat: add componente for display artist info

---
+++ components/elements/ArtistResultItem.tsx
@@ -0,0 +1,41 @@
+import { useColorMode, useColorModeValue } from "@chakra-ui/color-mode";
+import { Image } from "@chakra-ui/image";
+import { Box, Flex, Spacer } from "@chakra-ui/layout";
+import React from "react";
+
+type ArtistResultItemProps = {
+  artistName: string;
+  profilePictureURL: string;
+};
+
+function ArtistResultItem({
+  artistName,
+  profilePictureURL
+}: ArtistResultItemProps) {
+  const { colorMode } = useColorMode();
+  return (
+    <Flex
+      _hover={{
+        cursor: "pointer"
+      }}
+      height='80px'
+      layerStyle={colorMode === "dark" ? "cardDarkMode" : "cardLightMode"}
+      width='100%'
+      borderRadius='md'
+      p={5}
+      alignItems='center'
+    >
+      {artistName}
+      <Spacer></Spacer>
+      <Image
+        borderRadius='full'
+        boxSize='60px'
+        alt='Artist Cover'
+        fallbackSrc='img/mello-face.png'
+        src={profilePictureURL}
+      />
+    </Flex>
+  );
+}
+
+export default ArtistResultItem;
