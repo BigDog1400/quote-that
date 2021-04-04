8f7e007028c0e7f8bb7a7c288f09f980be7ee2f5
Isaac Peña <isaac.pr99@gmail.com>
2021-04-04 02:00:02 -0400

    refactor: move logic for rendering to another component

---
+++ components/templates/ArtistDetails.tsx
@@ -0,0 +1,41 @@
+import { Flex, VStack } from "@chakra-ui/layout";
+import React, { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../../lib/reduxHooks";
+import {
+  fetchArtist,
+  resultArtist,
+  statusSearchArtist
+} from "../../lib/slices/artistSlice";
+import AlbumListResult from "../modules/AlbumListResult";
+import ArtistAvatar from "../modules/ArtistAvatar";
+
+type ArtistDetailsProps = {
+  idArtist: string;
+};
+
+function ArtistDetails({ idArtist }: ArtistDetailsProps) {
+  const dispatch = useAppDispatch();
+  const artistData = useAppSelector(resultArtist);
+  const currentStatusSearchArtist = useAppSelector(statusSearchArtist);
+
+  useEffect(() => {
+    dispatch(fetchArtist({ search: idArtist, getAlbums: true }));
+  }, [idArtist]);
+  return (
+    <VStack spacing='2rem'>
+      <ArtistAvatar
+        urlAvatar={artistData?.cover}
+        artistName={artistData?.artist}
+        isLoading={
+          currentStatusSearchArtist === "loading" ||
+          currentStatusSearchArtist === "idle"
+        }
+      />
+      <Flex wrap='wrap' justifyContent='center'>
+        <AlbumListResult />
+      </Flex>
+    </VStack>
+  );
+}
+
+export default ArtistDetails;
