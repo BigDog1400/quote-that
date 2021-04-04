7717289c1cdecac663d4131ac4f5210fe009d5ee
Isaac Peña <isaac.pr99@gmail.com>
2021-04-03 22:47:40 -0400

    feat: add component for identifying artist details

---
+++ components/elements/AvatarWithName.tsx
@@ -0,0 +1,29 @@
+import { Avatar } from "@chakra-ui/avatar";
+import { useColorMode } from "@chakra-ui/color-mode";
+import { Box, Heading, Text, VStack } from "@chakra-ui/layout";
+import React from "react";
+
+type AvatarWithNameProps = {
+  name: string;
+  urlAvatar: string;
+};
+
+function AvatarWithName({ name, urlAvatar }: AvatarWithNameProps) {
+  const { colorMode } = useColorMode();
+  return (
+    <VStack spacing='2rem'>
+      <Box height='200px' w='200px'>
+        <Avatar
+          showBorder
+          borderColor={colorMode === "dark" ? "white" : "black"}
+          size='full'
+          name={name}
+          src={urlAvatar}
+        />
+      </Box>
+      <Heading as='h3'>{name}</Heading>
+    </VStack>
+  );
+}
+
+export default AvatarWithName;
