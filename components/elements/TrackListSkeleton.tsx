b73b2c2f4e23cb51be4d1ebe184f67b249d65ff1
Isaac Peña <isaac.pr99@gmail.com>
2021-04-04 22:37:09 -0400

    feat: add skeleton for track list item

---
+++ components/elements/TrackListSkeleton.tsx
@@ -0,0 +1,9 @@
+import { HStack } from "@chakra-ui/layout";
+import { Skeleton } from "@chakra-ui/skeleton";
+import React from "react";
+
+function TrackListSkeleton() {
+  return <Skeleton height='60px' w='100%'></Skeleton>;
+}
+
+export default TrackListSkeleton;
