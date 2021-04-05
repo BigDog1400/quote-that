b98a2470bf52483c0ea1b143d3776c698cfcd0e6
Isaac Peña <isaac.pr99@gmail.com>
2021-04-04 22:51:56 -0400

    feat(ArtistResultItem): remove border radius

--- components/elements/ArtistResultItem.tsx
+++ components/elements/ArtistResultItem.tsx
@@ -23,7 +23,6 @@ function ArtistResultItem({
       height='80px'
       layerStyle={colorMode === "dark" ? "cardDarkMode" : "cardLightMode"}
       width='100%'
-      borderRadius='md'
       p={5}
       alignItems='center'
     >
