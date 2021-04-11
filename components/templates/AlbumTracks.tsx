6173025a8c8a3501e28eb592669cd6d85a5f0cf0
Isaac Peña <isaac.pr99@gmail.com>
2021-04-10 22:28:38 -0400

    feat: add spacing and margin

--- components/templates/AlbumTracks.tsx
+++ components/templates/AlbumTracks.tsx
@@ -1,5 +1,5 @@
 import { Image } from "@chakra-ui/image";
-import { Box, Center, Flex, Heading, Text } from "@chakra-ui/layout";
+import { Box, Center, Flex, Heading, Stack, Text } from "@chakra-ui/layout";
 import React from "react";
 import AlbumDetails from "../elements/AlbumDetails";
 import AlbumDetailsResult from "../modules/AlbumDetailsResult";
@@ -12,12 +12,12 @@ type AlbumTracksProps = {
 
 function AlbumTracks({ idArtist, idAlbum }: AlbumTracksProps) {
   return (
-    <>
+    <Stack spacing='3rem' marginTop='2rem' marginBottom='4rem'>
       <AlbumDetailsResult idAlbum={idAlbum} idArtist={idArtist} />
       <Center mt={4}>
         <AlbumTrackListResult idAlbum={idAlbum} idArtist={idArtist} />
       </Center>
-    </>
+    </Stack>
   );
 }
 
