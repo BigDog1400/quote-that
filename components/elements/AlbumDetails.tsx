2735456cf8738ef49e2c21d2c3748b8a427a9cec
Isaac Peña <isaac.pr99@gmail.com>
2021-04-10 22:11:22 -0400

    feat: :sparkles: add routing event in album details to artist path

--- components/elements/AlbumDetails.tsx
+++ components/elements/AlbumDetails.tsx
@@ -1,5 +1,6 @@
 import { Image } from "@chakra-ui/image";
 import { Box, Flex, Heading, Text } from "@chakra-ui/layout";
+import { useRouter } from "next/router";
 import React, { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../../lib/reduxHooks";
 
@@ -11,12 +12,24 @@ type AlbumsDetailsProps = {
 };
 
 function AlbumDetails({ cover, album, artist, realease }: AlbumsDetailsProps) {
+  const router = useRouter();
+  const { id_artist } = router.query;
   return (
     <Flex spacing='4'>
       <Image src={cover} height='200px'></Image>
       <Box paddingLeft='2rem'>
         <Heading>{album}</Heading>
-        <Text fontSize='2xl'>{artist}</Text>
+        <Text
+          _hover={{
+            textDecoration: "underline",
+            fontStyle: "italic",
+            cursor: "pointer"
+          }}
+          fontSize='2xl'
+          onClick={() => router.push("/artist/" + id_artist)}
+        >
+          {artist}
+        </Text>
         {realease ? <Text fontSize='sm'>Realease Date: {realease}</Text> : null}
       </Box>
     </Flex>
